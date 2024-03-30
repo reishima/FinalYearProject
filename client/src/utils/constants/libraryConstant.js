919b5274fa5719fc358baa7811c6b9f30bf1b3ea
reishima <116885615+reishima@users.noreply.github.com>
2024-03-30 13:48:01 +0800

    Update libraryConstant.js

--- client/src/utils/constants/libraryConstant.js
+++ client/src/utils/constants/libraryConstant.js
@@ -1,4 +1,4 @@
 import abi from '../LibraryBook.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = '0xe702e6db78a336ab0bba6391a16a6162a92d3d61';
+export const contractAddress = '';
