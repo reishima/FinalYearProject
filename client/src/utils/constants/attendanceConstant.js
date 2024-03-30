c2dcf82e5a3ef895e2cab930b14e2e400ae33983
reishima <116885615+reishima@users.noreply.github.com>
2024-03-30 13:47:53 +0800

    Update attendanceConstant.js

--- client/src/utils/constants/attendanceConstant.js
+++ client/src/utils/constants/attendanceConstant.js
@@ -1,4 +1,4 @@
 import abi from '../CourseAttendance.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = '0x39c1a33215c1f36d248bad21c23ab7c2cb2c6c29';
+export const contractAddress = '';
