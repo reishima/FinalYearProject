30fb26c11e674d00b0c738297503f33c1ce63502
reishima <116885615+reishima@users.noreply.github.com>
2024-03-30 13:47:43 +0800

    Update aideConstant.js

--- client/src/utils/constants/aideConstant.js
+++ client/src/utils/constants/aideConstant.js
@@ -1,4 +1,4 @@
 import abi from '../StudentAide.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = '0x515df72754c65a7adf1fbb3ef101107b8ebbb7cd';
+export const contractAddress = '';
